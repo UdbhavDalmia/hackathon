0573421329c8eaebd96a106130ebb247acf3bf09
Udbhav <udbhavdalmia123@gmail.com>
2026-05-02 14:34:21 +0530

    login and new icon

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function updateTime() {
     const date = new Date()
     const hours = date.getHours() % 12 || 12 // Changes to PM after 13
     const minutes = date.getMinutes().toString().padStart(2, '0')
+    const seconds = date.getSeconds().toString().padStart(2, '0')
     const ampm = date.getHours() >= 12 ? 'PM' : 'AM' // Changes AM/PM
     time_element.textContent = `${hours}:${minutes} ${ampm}`
 
