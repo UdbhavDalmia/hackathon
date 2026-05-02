9c556071716282282c223bd29194e39154ac4404
Udbhav <udbhavdalmia123@gmail.com>
2026-05-02 11:44:42 +0530

    id changes

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const date = document.querySelector('#date')
-const time = document.querySelector("#time")
+const date_element = document.querySelector('#date_element')
+const time_element = document.querySelector("#time_element")
 
 function updateTime() {
-    const now = new Date()
-    const hours = now.getHours() % 12 || 12 // Changes to PM after 13
-    const minutes = now.getMinutes().toString().padStart(2, '0')
-    const ampm = now.getHours() >= 12 ? 'PM' : 'AM' // Changes AM/PM
-    time.textContent = `${hours}:${minutes} ${ampm}`
+    const date = new Date()
+    const hours = date.getHours() % 12 || 12 // Changes to PM after 13
+    const minutes = date.getMinutes().toString().padStart(2, '0')
+    const ampm = date.getHours() >= 12 ? 'PM' : 'AM' // Changes AM/PM
+    time_element.textContent = `${hours}:${minutes} ${ampm}`
 
     const options = { month: 'short', day: 'numeric' }
-    date.textContent = now.toLocaleDateString(undefined, options)
+    date_element.textContent = date.toLocaleDateString(undefined, options)
 }
 
 updateTime()
